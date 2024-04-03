89de731ad5493591d28396d7c1b18c107f5ee5ff
AkashKapoor <akashkapoor306@gmail.com>
2024-04-03 19:00:32 -0400

    Added Animation to Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour{
    
    [SerializeField] private float moveSpeed = 7f;
+   
+   bool isWalking;
     private void Update() {
         Vector2 inputVector = new Vector2(0,0);
         if(Input.GetKey(KeyCode.W))
@@ -27,9 +29,17 @@ public class Player : MonoBehaviour{
 
         movDir  = movDir.normalized;
 
+        isWalking = movDir != Vector3.zero;
+
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward,movDir,Time.deltaTime * rotateSpeed);
         
         transform.position += movDir * moveSpeed * Time.deltaTime;
+    }
+
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
