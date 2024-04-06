b60b94cc33ecf3847944b73a64e7b57404f91bc5
AkashKapoor <akashkapoor306@gmail.com>
2024-04-06 19:45:40 -0400

    Refactor InputSystem

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+    private PlayerInputActions playerInputAction;
+
+    private void Awake() {
+        playerInputAction = new PlayerInputActions();
+        playerInputAction.Player.Enable();
+    }
+
+    public Vector2 GetMovementNormalized()
+    {
+        Vector2 inputVector = playerInputAction.Player.Move.ReadValue<Vector2>();
+
+        inputVector = inputVector.normalized;
+
+        return inputVector;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,35 +5,16 @@ using UnityEngine;
 public class Player : MonoBehaviour{
    
    [SerializeField] private float moveSpeed = 7f;
+   [SerializeField] private GameInput gameInput;
    
    bool isWalking;
     private void Update() {
-        Vector2 inputVector = new Vector2(0,0);
-        if(Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if(Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if(Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if(Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
+        Vector2 inputVector = gameInput.GetMovementNormalized();
         Vector3 movDir = new Vector3(inputVector.x,0,inputVector.y);
-
-        movDir  = movDir.normalized;
-
+        
         isWalking = movDir != Vector3.zero;
-
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward,movDir,Time.deltaTime * rotateSpeed);
-        
         transform.position += movDir * moveSpeed * Time.deltaTime;
     }
 
